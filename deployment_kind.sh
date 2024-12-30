0c22be8adf53422437a9d3495f8cee4874e1b958
paco navarrete <paco.j.navarrete@gmail.com>
2024-12-30 14:42:32 +0000

    addedd support for multiple namespaces

--- deployment_kind.sh
+++ deployment_kind.sh
@@ -3,27 +3,28 @@
 # author: francisco josé navarrete pan paco.j.navarrete@gmail.com
 # Functions
 
-deploy_kind_cluster () {
-  echo -e "deploying $FUNCNAME... "
-  nvkind cluster create --name=$2 --config-template=data/kind/kind-gpu.yaml
-
-#  nvkind create cluster -n $2 --config data/kind/kind-gpu.yaml
-  # TODO: add ingress and on values.yaml also
-  kubectl create namespace $2
-  install_ingress $1 $2
-  echo "DONE (check errors)"
-  sleep 5
-}
+# PREFLIGHT
+cp -pr data data_$2
+cd data_$2
+find . -name "*.yaml" | xargs perl -pi -e "s/..WORKSPACE../$2/g" 
+cd ..
 
 deploy_namespace () {
   echo -e "deploying $FUNCNAME... "
-
   kubectl create namespace $2
-  install_ingress $1 $2
-  deploy_metricserver $1 $2
   echo "DONE (check errors)"
   sleep 5
 
+}
+
+deploy_kind_cluster () {
+  echo -e "deploying $FUNCNAME... "
+  nvkind cluster create --name=$2 --config-template=data_$2/kind/kind-gpu.yaml
+  # TODO: add ingress and on values.yaml also
+  deploy_namespace $1 $2
+  install_ingress $1 $2
+  echo "DONE (check errors)"
+  sleep 5
 }
 
 test_gpu_use () {
@@ -47,7 +48,7 @@ EOF
 
 install_ingress () {
   echo -e "deploying $FUNCNAME... "
-  kubectl apply -f data/ingress/deploy-ingress-nginx.yaml
+  kubectl apply -f data_$2/ingress/deploy-ingress-nginx.yaml
   # kubectl apply -f https://kind.sigs.k8s.io/examples/ingress/deploy-ingress-nginx.yaml
   sleep 5
   kubectl wait --namespace ingress-nginx \
@@ -63,14 +64,14 @@ deploy_metricserver() {
   echo -e "deploying $FUNCNAME... "
   helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
   helm repo update
-  helm upgrade --install metrics-server metrics-server/metrics-server -f data/metrics-server/values.yaml
+  helm upgrade --install metrics-server metrics-server/metrics-server -f data_$2/metrics-server/values.yaml
   echo "DONE (check errors)"
 }
 
 install_ollama () {
   echo -e "deploying $FUNCNAME... "
   helm repo add cowboysysop https://cowboysysop.github.io/charts/
-  helm upgrade --install ollama cowboysysop/ollama --namespace $2 -f data/ollama/values.yaml
+  helm upgrade --install ollama cowboysysop/ollama --namespace $2 -f data_$2/ollama/values.yaml
   echo "DONE (check errors)"
 }
 
@@ -79,7 +80,7 @@ install_open-webui () {
     echo -e "deploying $FUNCNAME... "
     helm repo add open-webui https://helm.openwebui.com/
     helm repo update
-    helm upgrade -i openwebui open-webui/open-webui --namespace $2 -f data/open-webui/values.yaml
+    helm upgrade -i openwebui open-webui/open-webui --namespace $2 -f data_$2/open-webui/values.yaml
     echo "DONE (check errors)"
 }
 
@@ -89,11 +90,11 @@ install_kafka () {
   helm repo add licenseware https://licenseware.github.io/charts/
   helm repo update
 
-  helm upgrade -i kafka oci://registry-1.docker.io/bitnamicharts/kafka -f data/kafka/values.yaml --namespace $2
-  helm upgrade -i schema-registry  oci://registry-1.docker.io/bitnamicharts/schema-registry -f data/schema-registry/values.yaml --namespace $2
+  helm upgrade -i kafka oci://registry-1.docker.io/bitnamicharts/kafka -f data_$2/kafka/values.yaml --namespace $2
+  helm upgrade -i schema-registry  oci://registry-1.docker.io/bitnamicharts/schema-registry -f data_$2/schema-registry/values.yaml --namespace $2
   # TODO: repair schema-registry
-  #helm upgrade -i schema-registry-ui cloudnativeapp/schema-registry-ui -f data/schema-registry-ui/values.yaml --namespace $2
-  helm upgrade -i ksqldb licenseware/ksqldb -f data/ksqldb/values.yaml --namespace $2
+  #helm upgrade -i schema-registry-ui cloudnativeapp/schema-registry-ui -f data_$2/schema-registry-ui/values.yaml --namespace $2
+  helm upgrade -i ksqldb licenseware/ksqldb -f data_$2/ksqldb/values.yaml --namespace $2
   echo "DONE (check errors)"
 }
 
@@ -102,7 +103,7 @@ install_vector () {
   echo -e "deploying $FUNCNAME... "
   helm repo add qdrant https://qdrant.github.io/qdrant-helm
   helm repo update
-  helm upgrade -i qdrant qdrant/qdrant -n $2 -f data/qdrant/values.yaml
+  helm upgrade -i qdrant qdrant/qdrant -n $2 -f data_$2/qdrant/values.yaml
   echo "DONE (check errors)"
 }
 
@@ -127,6 +128,7 @@ if [ "$1" == "nvkind" ]
 then
   #local_set_gpu_use $1 $2 
   deploy_kind_cluster $1 $2
+  deploy_metricserver $1 $2 &
   #post_kind_cluster_gpu $1 $2
   #test_gpu_use $1 $2 
 fi
@@ -147,7 +149,6 @@ then
   exit
 fi
 
-deploy_metricserver $1 $2 &
 install_ollama $1 $2
 install_open-webui $1 $2
 install_kafka $1 $2
